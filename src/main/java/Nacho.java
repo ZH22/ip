9f661281b7033da8d2142c924f8c304b8c562244
zhenghao <zhenghao3210@gmail.com>
2025-08-17 18:10:26 +0800

    Added Echo Feature

--- src/main/java/Nacho.java
+++ src/main/java/Nacho.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Nacho {
     public static void main(String[] args) {
 
-        String horizontal_line = "----------------------------------";
+        // Visual Elements
+        String horizontal_line = "_____________________________________";
 
+        // Greeting
         System.out.println(horizontal_line);
         System.out.println("Hello I'm Nacho\nWhat can I do for you?");
+        System.out.println(horizontal_line);
+
+        // Echo
+        Scanner scanner = new Scanner(System.in);
+        String query = scanner.nextLine();
+
+        while(!query.equals("bye")) {
+            System.out.println(horizontal_line);
+            System.out.println(query);
+            System.out.println(horizontal_line);
+
+            query = scanner.nextLine();
+        }
+
+        // Exit
         System.out.println(horizontal_line);
         System.out.println("Bye. Hope to see you soon!");
         System.out.println(horizontal_line);
