e00efc520361bb9040e951d7592b173fbcceb356
zhenghao <zhenghao3210@gmail.com>
2025-08-17 17:53:44 +0800

    Renamed Duke to Nacho, Added Greeting and Exit

---
+++ src/main/java/Nacho.java
@@ -0,0 +1,12 @@
+public class Nacho {
+    public static void main(String[] args) {
+
+        String horizontal_line = "----------------------------------";
+
+        System.out.println(horizontal_line);
+        System.out.println("Hello I'm Nacho\nWhat can I do for you?");
+        System.out.println(horizontal_line);
+        System.out.println("Bye. Hope to see you soon!");
+        System.out.println(horizontal_line);
+    }
+}
