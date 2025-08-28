243771c87a4bdc0b0fcb7f71b53b3aba04614840
zhenghao <zhenghao3210@gmail.com>
2025-08-28 13:14:26 +0800

    Add JUnit Basic Tests for TodoTask and DeadlineTask

---
+++ src/test/java/Nacho/Tasks/DeadlineTaskTest.java
@@ -0,0 +1,31 @@
+package Nacho.Tasks;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTaskTest {
+    @Test
+    public void testStringConversion() {
+        DeadlineTask testTask = new DeadlineTask("Test", "08/12/2025-23:40");
+
+        // Standard String Conversion Format
+        assertEquals("[D][ ] Test (by: 08 December 2025 - 11:40 PM)", testTask.toString());
+
+        // Storage String Conversion Format
+        assertEquals("D | 0 | Test | 08/12/2025-23:40", testTask.getStorageRepresentation());
+    }
+
+    @Test
+    public void testMarkAndUnmark() {
+        DeadlineTask testTask = new DeadlineTask("Test", "08/12/2025-23:40");
+
+        testTask.markCompleted();
+        assertEquals("[D][X] Test (by: 08 December 2025 - 11:40 PM)", testTask.toString());
+        assertEquals(1, testTask.isCompleted());
+
+        testTask.unmarkCompleted();
+        assertEquals("[D][ ] Test (by: 08 December 2025 - 11:40 PM)", testTask.toString());
+        assertEquals(0, testTask.isCompleted());
+    }
+
+}
---
+++ src/test/java/Nacho/Tasks/TodoTaskTest.java
@@ -0,0 +1,25 @@
+package Nacho.Tasks;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoTaskTest {
+    @Test
+    public void testStringConversion() {
+        assertEquals("[T][ ] Test Title",new TodoTask("Test Title").toString() );
+    }
+
+    @Test
+    public void testMarkAndUnmark() {
+        TodoTask testTask = new TodoTask("Test");
+
+        testTask.markCompleted();
+        assertEquals("[T][X] Test", testTask.toString());
+        assertEquals(1, testTask.isCompleted());
+
+        testTask.unmarkCompleted();
+        assertEquals("[T][ ] Test", testTask.toString());
+        assertEquals(0, testTask.isCompleted());
+    }
+
+}
